c01297cbb85a97f2a7f96d681be39316362aafc0
Vasil <vsilt2002@gmail.com>
2024-07-06 18:46:37 +0200

    Integrating another project

---
+++ CarRental.Web/Controllers/RestaurantsController.cs
@@ -0,0 +1,18 @@
+﻿using CarRental.Domain.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRental.Web.Controllers
+{
+    public class RestaurantsController : Controller
+    {
+        public IActionResult Index()
+        {
+            HttpClient client = new HttpClient();
+            string URL = "https://integriraniproject.azurewebsites.net/api/Admin/GetAllRestaurants";
+
+            HttpResponseMessage response = client.GetAsync(URL).Result;
+            var data = response.Content.ReadAsAsync<List<Restaurants>>().Result;
+            return View(data);
+        }
+    }
+}

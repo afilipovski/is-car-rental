63651addcd78e2ec6084930c46c9c3882e5b4479
srbinovskia46 <andrej.srbinovski@students.finki.ukim.mk>
2024-07-06 18:33:08 +0200

    Added api controller

---
+++ CarRental.Web/Controllers/api/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using CarRental.Domain.Domain;
+using CarRental.Service.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRental.Web.Controllers.api
+{
+    namespace CarRental.Web.Controllers.API
+    {
+        [Route("api/[controller]")]
+        [ApiController]
+        public class AdminController : ControllerBase
+        {
+            private readonly ICarService _carService;
+            public AdminController(ICarService carService)
+            {
+                _carService = carService;
+            }
+
+
+
+            [HttpGet("[action]")]
+            public List<Car> GetAllCars()
+            {
+                return this._carService.GetCars();
+            }
+        }
+    }
+}
